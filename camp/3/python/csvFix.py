71d1fc9f64caf3a44295345fcf275c5b5c97bb32
harutiro <harutiro2027@outlook.jp>
2023-09-19 09:43:14 +0900

    正解データを用意した。

---
+++ camp/3/python/csvFix.py
@@ -0,0 +1,25 @@
+import csv
+from datetime import datetime
+
+input_file = "camp/3/data/火おこし4/ans.csv"
+output_file = "camp/3/data/火おこし4/ans.csv"
+
+datetime_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+with open(input_file, "r", newline="") as file:
+    reader = csv.DictReader(file)
+    rows = list(reader)
+
+    for row in rows:
+        datetime_str = row["time"]
+        datetime_obj = datetime.strptime(datetime_str, datetime_format)
+        absolute_time_ms = int(datetime_obj.timestamp() * 1000)
+        row["time"] = absolute_time_ms
+
+with open(output_file, "w", newline="") as file:
+    fieldnames = reader.fieldnames
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(rows)
+
+print("変換が完了しました。")
